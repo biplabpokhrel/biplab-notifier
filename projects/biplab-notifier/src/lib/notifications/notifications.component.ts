71763c4a08b33c655145ffab94d0609354631449
Biplab Pokhrel <biplav.nep@gmail.com>
2019-03-12 02:35:12 -0400

    Added a option to pass multiple buttons

--- projects/biplab-notifier/src/lib/notifications/notifications.component.ts
+++ projects/biplab-notifier/src/lib/notifications/notifications.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit, Input, Output, EventEmitter, ElementRef, ViewChild  
 import { NotificationHint } from '../layout/notifier';
 import { Message } from '../message/notifer';
 import { isArray } from 'util';
-import { MultiNotifier } from '../layout/notifier';
+import { MultiNotifier, NotificationButton } from '../layout/notifier';
+
 
 @Component({
   selector: 'biplab-notifications',
@@ -12,10 +13,10 @@ import { MultiNotifier } from '../layout/notifier';
 export class NotificationsComponent implements OnInit {
   @ViewChild('parentDailog') myDiv: ElementRef;
   @Input() layoutHint: NotificationHint;
-  @Output() close: EventEmitter<boolean>;
+  @Output() close: EventEmitter<any>;
   data: Message[];
   constructor() {
-    this.close = new EventEmitter<boolean>();
+    this.close = new EventEmitter<any>();
   }
 
   ngOnInit() {
@@ -35,12 +36,30 @@ export class NotificationsComponent implements OnInit {
     }
   }
 
-  notificationClose(status: boolean) {
+  click(button: NotificationButton) {
+    if (!!button.callBackFunctions) {
+      for (const custom of button.callBackFunctions) {
+        if ( custom.param) {
+          custom.func(custom.param);
+        } else {
+          custom.func();
+        }
+      }
+    } else {
+      this.notificationClose(button.emitValue);
+    }
+  }
+  notificationClose(status: any) {
     this.close.emit(status);
   }
 
   get shadow(): string {
     return !!this.layoutHint.css.shadow ? `${ this.layoutHint.layout.displayAs }-shadow` : '';
   }
+
+  buttonStatus(button: NotificationButton): boolean {
+    return typeof(button.emitValue) === 'boolean' && button.emitValue;
+  }
+
 
 }
--- projects/biplab-notifier/src/public_api.ts
+++ projects/biplab-notifier/src/public_api.ts
@@ -7,3 +7,5 @@ export * from './lib/notifier/notifier.component';
 export * from './lib/notifier.module';
 export * from './lib/notification';
 export * from './lib/template/notifier';
+export * from './lib/layout/notifier';
+
